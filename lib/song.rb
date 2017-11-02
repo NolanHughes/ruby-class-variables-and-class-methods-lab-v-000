66195ad3c884e9be7e2558e120209609a0aa47c7
Nolan Hughes <nhughes987@gmail.com>
2017-11-02 18:57:09 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -32,26 +32,8 @@ class Song
   end
 
   def self.genre_count
-    # number_of_times = []
-    # @@genres_array.each do |type|
-    #   number_of_times << @@genres_array.count(type)
-    #   @@genres_count[type] = count #count var
-    # end
     @@genres.group_by{ |v| v }.map{ |k, v| [k, v.size] }.to_h
   end
 
 end
 
-###Work code###
-# genres_array = ["rap", "rap", "pop"]
-# number_of_times = []
-#
-# genres_array.each do |type|
-#   number_of_times << genres_array.count(type)
-# end
-#
-# number_of_times
-#
-# trial_hash = {}
-# data = [0,1,2,2,2,2,2,3,3,3,3,3,3,4,4,4,4,5,5,6,6,6,7,7,7,7,7,8,9,9,10]
-# Hash[*data.group_by{ |v| v }.flat_map{ |k, v| [k, v.size] }]
